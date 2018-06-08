aa7c509fed37c6b370d1ed1d1af418fcbb09a60b
omniaMagd93 <omniamagd1993@gmail.com>
2018-06-08 16:47:35 +0200

    add seed

--- IMS/db/seeds.rb
+++ IMS/db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+Track.create!(name: 'os',intake: '38',typeoftrack: '9 month') if Rails.env.development?
+Group.create!(name: 'A') if Rails.env.development?
