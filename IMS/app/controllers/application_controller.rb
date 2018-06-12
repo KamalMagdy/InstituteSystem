0a6b9c2897284b085605a84ca6b2805df6e6d6fe
KamalMagdy <kamal.m2331@yahoo.com>
2018-06-12 03:42:44 +0200

    add basic ChatBot + enhancement + bugs fix

--- IMS/app/controllers/application_controller.rb
+++ IMS/app/controllers/application_controller.rb
@@ -6,9 +6,12 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
       devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     end
-  # 	def after_sign_in_path_for(resource)
-		# '/students/edit'
-  # 	end
+
+   	def after_sign_in_path_for(resource)
+      if current_admin_user.role=="Instructor"
+	    redirect '/home'
+  	  end
+    end
     def current_ability
       @current_ability ||= Ability.new(current_student)
     end
--- IMS/db/schema.rb
+++ IMS/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_09_132009) do
+ActiveRecord::Schema.define(version: 2018_06_11_233507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2018_06_09_132009) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "role"
-    t.integer "social_no"
+    t.string "social_no"
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
